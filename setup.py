70739192aa084308c51d712ad1ffdfb576b883dc
amosbastian <amos_bastian@hotmail.com>
2018-05-14 00:24:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="football",
     version="0.1.0",
-    description="A python wrapper around the football-data API.",
+    description="A Python wrapper around the football-data API.",
     url="https://github.com/amosbastian/football",
     author="Amos Bastian",
     author_email="amosbastian@googlemail.com",
@@ -12,8 +12,9 @@ setup(
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU Affero General Public License v3",
-        "Programming Language :: Python"
+        "Programming Language :: Python :: 3.6"
     ],
-    keywords="football",
-    install_requires=["requests"]
+    keywords="football football-data api",
+    install_requires=["requests"],
+    python_requires=">=3.6"
 )
