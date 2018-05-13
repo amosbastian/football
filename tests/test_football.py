3532cb0285029171959a03a5f9912f4c598f4040
amosbastian <amos_bastian@hotmail.com>
2018-05-13 17:56:35 +0200

    Add unit testing for filters

--- tests/test_football.py
+++ tests/test_football.py
@@ -7,6 +7,7 @@ class TestFootball(unittest.TestCase):
     football = Football(os.environ["FOOTBALL_API_KEY"])
 
     def test_competitions(self):
+        # General tests
         competitions = self.football.competitions()
         self.assertIsInstance(competitions, list)
         self.assertEqual(competitions[0]["id"], 444)
@@ -16,7 +17,13 @@ class TestFootball(unittest.TestCase):
         self.assertEqual(competitions[0]["numberOfTeams"], 20)
         self.assertEqual(competitions[0]["numberOfGames"], 380)
 
+        # Test with query parameters
+        competitions = self.football.competitions(2015)
+        self.assertEqual(competitions[0]["year"], "2015")
+        self.assertRaises(ValueError, self.football.competitions, "abc")
+
     def test_teams(self):
+        # General tests
         teams = self.football.teams(445)
         self.assertIsInstance(teams, dict)
         self.assertEqual(teams["count"], 20)
@@ -24,28 +31,51 @@ class TestFootball(unittest.TestCase):
         self.assertEqual(teams["count"], len(teams["teams"]))
 
     def test_table(self):
+        # General tests
         table = self.football.table(445)
         self.assertIsInstance(table, dict)
         self.assertIsInstance(table["standing"], list)
         self.assertEqual(len(table["standing"]), 20)
 
+        # Test with query parameters
+        table = self.football.table(445, 1)
+        self.assertEqual(table["matchday"], 1)
+        self.assertRaises(ValueError, self.football.table, 445, "abc")
+
     def test_competition_fixtures(self):
+        # General tests
         competition_fixtures = self.football.competition_fixtures(445)
         self.assertIsInstance(competition_fixtures, dict)
         self.assertIsInstance(competition_fixtures["fixtures"], list)
         self.assertEqual(competition_fixtures["count"],
                          len(competition_fixtures["fixtures"]))
 
+        # Test with query parameters
+        self.assertRaises(
+            ValueError, self.football.competition_fixtures, 445, "abc")
+        self.assertEqual(
+            self.football.competition_fixtures(445, matchday=38),
+            self.football.competition_fixtures(445, time_frame="n38"))
+
     def test_fixtures(self):
+        # General tests
         fixtures = self.football.fixtures()
         self.assertIsInstance(fixtures, dict)
         self.assertIsInstance(fixtures["fixtures"], list)
         self.assertEqual(fixtures["count"], len(fixtures["fixtures"]))
 
+        # Test with query parameters
+        self.assertRaises(ValueError, self.football.fixtures, time_frame="abc")
+        self.assertRaises(ValueError, self.football.fixtures, league_code=123)
+        self.assertEqual(
+            self.football.fixtures(league_code="PL")["fixtures"],
+            self.football.competition_fixtures(445, matchday=38)["fixtures"])
+
     def test_fixture(self):
         pass
 
     def test_team_fixtures(self):
+        # General tests
         team_fixtures = self.football.team_fixtures(66)
         self.assertIsInstance(team_fixtures, dict)
         self.assertIsInstance(team_fixtures["fixtures"], list)
@@ -53,7 +83,16 @@ class TestFootball(unittest.TestCase):
                          team_fixtures["count"])
         self.assertEqual(team_fixtures["fixtures"][0]["matchday"], 1)
 
+        # Test with query parameters
+        self.assertRaises(
+            ValueError, self.football.team_fixtures, 66, time_frame="abc")
+        self.assertRaises(
+            ValueError, self.football.team_fixtures, 66, season="abc")
+        self.assertRaises(
+            ValueError, self.football.team_fixtures, 66, venue="abc")
+
     def test_team(self):
+        # General tests
         team = self.football.team(66)
         self.assertIsInstance(team, dict)
         self.assertEqual(team["name"], "Manchester United FC")
@@ -61,12 +100,14 @@ class TestFootball(unittest.TestCase):
         self.assertEqual(team["shortName"], "ManU")
 
     def test_players(self):
+        # General tests
         players = self.football.players(66)
         self.assertIsInstance(players, dict)
         self.assertIsInstance(players["players"], list)
         self.assertEqual(len(players["players"]), players["count"])
 
     def test__generate_url(self):
+        # General tests
         url = self.football._generate_url("competitions")
         self.assertEqual(url, "http://api.football-data.org/v1/competitions/")
         url = self.football._generate_url("competitions", {"season": 2015})
