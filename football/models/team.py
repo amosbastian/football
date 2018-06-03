7700c1591fafeb2e9a54252ac0ff87f1e9c97bdd
amosbastian <amos_bastian@hotmail.com>
2018-06-03 23:37:52 +0200

    Add Team class, update football.py

---
+++ football/models/team.py
@@ -0,0 +1,38 @@
+import requests
+
+from .fixture import Fixture
+from .player import Player
+
+
+class Team():
+    """
+    The Team class.
+    """
+    def __init__(self, team):
+        self.team_url = team["_links"]["self"]["href"]
+        self.fixtures_url = team["_links"]["fixtures"]["href"]
+        self.players_url = team["_links"]["players"]["href"]
+        self.badge_url = team["crestUrl"]
+        self.team_id = int(self.team_url.split("/")[-1])
+
+        self.code = team["code"]
+        self.fixtures = self._fixtures()
+        self.name = team["name"]
+        self.players = self._players()
+        self.shortname = team["shortName"]
+        self.squad_value = team["squadMarketValue"]
+
+    def _players(self):
+        """
+        Returns a list of Player objects.
+        """
+        response = requests.get(self.players_url).json()
+        return [Player(player, self.team_id, self.name)
+                for player in response["players"]]
+
+    def _fixtures(self):
+        """
+        Returns a list of Fixture objects.
+        """
+        response = requests.get(self.fixtures_url).json()
+        return [Fixture(fixture) for fixture in response["fixtures"]]
